8f73d765f12a3d8fca4907f36c3449a36c8bf8b5
Michael Graz <mg.graz@gmail.com>
2018-05-29 22:22:31 -0700

    script to use new cdd as a test

---
+++ new-cdd.sh
@@ -0,0 +1,6 @@
+cdd_exe=/home/mike/base/development/cd-deluxe-build/main/_cdd
+if [[ -x $cdd_exe ]]
+then
+    function cdd { while read x; do eval $x >/dev/null; done < <(dirs -l -p | "${cdd_exe}" "$@"); }
+    alias cd=cdd
+fi
