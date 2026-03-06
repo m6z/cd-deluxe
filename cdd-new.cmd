6b2f0e9a91e7857b2cfa1d18bdbe5443fc5fdae8
M Graz <mggraz@gmail.com>
2026-03-06 09:30:46 -0800

    cleanup wave

--- cdd-new.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set cdd_exe=C:\Users\Mike\Development\cdd_v2\cd-deluxe\build1\main\Debug\cd-deluxe.exe
-set pushd_tmp=%TEMP%\pushd.tmp
-set cdd_tmp_cmd=%TEMP%\cdd.tmp.cmd
-pushd > %pushd_tmp%
-"%cdd_exe%" %* < %pushd_tmp% > %cdd_tmp_cmd%
-%cdd_tmp_cmd%
